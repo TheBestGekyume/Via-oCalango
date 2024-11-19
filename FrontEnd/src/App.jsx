53a96b9ed90cf69f9358e884d446327333094046
Tallyson95 <tallyson95@gmail.com>
2024-11-19 04:12:11 -0300

    Criação das rotas e autenticação de usuário.

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,26 +1,23 @@
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Home } from './pages/Home.jsx';
-import './App.css';
 import { Login } from './pages/Login.jsx';
+import { NavBar } from './components/NavBar';
+import './App.css';
 
 export function App() {
+  const [token, setToken] = useState(window.sessionStorage.getItem('token'));
+
   return (
-    <main style={{ display: 'flex' }} className="container-main">
-      <Router>
-        <nav className="barra-lateral">
-          <ul>
-            <li>
-              <Link to="/home">Home</Link>
-            </li>
-          </ul>
-        </nav>
+    <Router>
+      <main style={{ display: 'flex' }} className="container-main">
+        {token && <NavBar setToken={setToken} />}
 
         <Routes>
-          <Route path="/" element={<Login />} />
+          <Route path="/" element={<Login setToken={setToken} />} />
           <Route path="/home" element={<Home />} />
         </Routes>
-      </Router>
-    </main>
+      </main>
+    </Router>
   );
 }
-
