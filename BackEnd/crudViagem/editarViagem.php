e1df4048d3b63d0cb56898ffef3c2e8e4febb650
Tallyson Silva <113393169+Tallyson95@users.noreply.github.com>
2024-11-24 03:56:39 -0300

    Revert "Iniciando as funcionalidades do adm"

--- BackEnd/crudViagem/editarViagem.php
+++ BackEnd/crudViagem/editarViagem.php
@@ -11,45 +11,29 @@ if ($_SERVER['REQUEST_METHOD'] == 'PUT') {
     $data = json_decode(file_get_contents("php://input"), true);
 
     // Verifica se os campos necessários estão definidos
-    if (isset($data['id_viagem'], $data['origem'], $data['destino'], 
-              $data['horario_de_partida'], $data['data_de_partida'], 
-              $data['preco'])) {
-
-        $id_viagem = $data['id_viagem'];
+    if (isset($data['id_viagem'], $data['origem'], $data['destino'], $data['horario_de_partida'], $data['data_de_partida'], $data['preco'])) {
+        $id_viagem = $data['id_viagem']; // ID do registro a ser editado
         $origem = $data['origem'];
         $destino = $data['destino'];
         $horario_de_partida = $data['horario_de_partida'];
         $data_de_partida = $data['data_de_partida'];
         $preco = $data['preco'];
 
+        // Monta a query de atualização
+        $sql = "UPDATE viagem 
+                SET origem = '$origem', destino = '$destino', 
+                    horario_de_partida = '$horario_de_partida', 
+                    data_de_partida = '$data_de_partida', 
+                    preco = '$preco' 
+                WHERE id_viagem = $id_viagem";
 
-        // Obtém os assentos atuais do banco de dados
-        $sql = "SELECT assentos FROM viagem WHERE id_viagem = $id_viagem";
-        $result = $conn->query($sql);
-
-        if ($result->num_rows > 0) {
-            $row = $result->fetch_assoc();
-
-            // Monta a query de atualização dos dados da viagem
-            $sql_update = "UPDATE viagem 
-                           SET origem = '$origem', 
-                               destino = '$destino', 
-                               horario_de_partida = '$horario_de_partida', 
-                               data_de_partida = '$data_de_partida', 
-                               preco = '$preco'
-                           WHERE id_viagem = $id_viagem";
-
-            // Executa a query de atualização
-            if ($conn->query($sql_update) === TRUE) {
-                echo "Viagem e assentos atualizados com sucesso!";
-            } else {
-                echo "Erro ao atualizar a viagem: " . $conn->error;
-            }
+        if ($conn->query($sql) === TRUE) {
+            echo "Viagem atualizada com sucesso!";
         } else {
-            echo "Viagem não encontrada!";
+            echo "Erro ao atualizar a viagem: " . $conn->error;
         }
     } else {
-        echo "Dados incompletos. Certifique-se de enviar todos os dados necessários (origem, destino, horário, data, preço, status e assentos).";
+        echo "Dados incompletos. Certifique-se de enviar 'id_viagem', 'origem', 'destino', 'horario_de_partida', 'data_de_partida' e 'preco'.";
     }
 }
 
